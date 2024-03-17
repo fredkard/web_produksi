d42f0317e2a792124853dd6d55622244211ec17f
fred kard <fred.kard@gmail.com>
2024-03-17 12:36:53 +0700

    Update index.php
    fixing table header

--- index.php
+++ index.php
@@ -596,15 +596,7 @@ while($rowop = mysqli_fetch_array($getop)){
 
                     </div>
 
-                    <!-- Content Row -->
-                    <div class="container-fluid">
-                        <div class="row">
-                            <!-- Page Heading -->
-                            <h1 class="h3 mb-2 text-gray-800">Tables</h1>
-                            <p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
-                            For more information about DataTables, please visit the <a target="_blank"
-                            href="https://datatables.net">official DataTables documentation</a>.</p>
-
+                    
 
                         <!-- Content Column ->
                         <div class="col-lg-6 mb-4">
