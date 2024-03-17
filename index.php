0ae6499fcac364a6ed098c64ec08f8b1aeb678f0
fred kard <fred.kard@gmail.com>
2024-03-17 13:23:13 +0700

    Update index.php
    add plugins for table js

--- index.php
+++ index.php
@@ -872,11 +872,16 @@ $retval_table = mysqli_query($conn,$sql );
 
     <!-- Page level plugins -->
     <script src="vendor/chart.js/Chart.min.js"></script>
+    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
     <!-- Page level custom scripts -->
     <script src="js/demo/chart-area-demo.js"></script>
     <script src="js/demo/chart-pie-demo.js"></script>
+    <script src="js/demo/datatables-demo.js"></script>
 
+
+	
 </body>
 
 </html>
